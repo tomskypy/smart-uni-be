df340d544b4b98763201529804b16eb97e71d97a
Tomáš Skýpala <tomas.skypala@kiwi.com>
2021-04-07 12:33:20 +0200

    feat: add faculties datasource

--- web/routes/get.js
+++ web/routes/get.js
@@ -1,9 +1,15 @@
 var router      = require('express').Router(),
-    qrpoints    = require('../res/qrpoints');
+    qrpoints    = require('../res/qrpoints'),
+    faculties   = require('../res/faculties');
 
 /* GET QR Points. */
 router.get('/qrpoints', function (req, res) {
     res.send(JSON.stringify(qrpoints));
+});
+
+/* GET Faculties. */
+router.get('/faculties', function (req, res) {
+    res.send(JSON.stringify(faculties));
 });
 
 module.exports = router;
